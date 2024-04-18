e6f7992b320b07304dd0d89f6c7d64d1fec96917
MehjabinKhan <mehjabinkhan2004@gmail.com>
2024-04-18 22:27:29 +0500

    While Loops

---
+++ loops.js
@@ -0,0 +1,17 @@
+"use strict";
+//    Loops
+let i = 0;
+while (i <= 2) {
+    console.log(i);
+    i++;
+}
+let num = 2;
+while (num <= 6) {
+    console.log(num);
+    num++;
+}
+let greek = 1;
+while (greek <= 2) {
+    console.log(greek);
+    greek++;
+}
